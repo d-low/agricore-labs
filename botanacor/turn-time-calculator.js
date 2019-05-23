21309476c3d9b994af251d11b08aaec30e75729e
Mike DiLorenzo <mikedilorenzo@gmail.com>
2019-05-22 22:01:12 -0600

    Add special case for Microbials - Friday after 10:30am

--- botanacor/turn-time-calculator.js
+++ botanacor/turn-time-calculator.js
@@ -121,13 +121,26 @@ var calculator = {
     var testDuration = this.TEST_DURATION[this.testType][this.submissionTime][this.turnAroundTime];
     var resultsDate = new Date(dropOffDayNormalized.getTime());
 
+    // Microbial tests dropped of on Friday after 10:30am cannot be processed
+    // until the following Monday with a test duration of 6 days. So reset the
+    // result date to Monday of the following week to begin the calculation.
+
+    if (this.testType === this.TEST_TYPES.MICROBIALS &&
+        this.submissionTime === 'AFTER 10:30AM' &&
+        dropOffDayNormalized.getDay() === 5) {
+      resultsDate = new Date(dropOffDayNormalized.getTime() + (2 * msPerDay));
+    }
+
     while (testDuration) {
       resultsDate = new Date(resultsDate.getTime() + msPerDay);
 
       if (this.testType === this.TEST_TYPES.MICROBIALS) {
+        // Microbial testing happens over the weekend so decrement the test duration.
         testDuration = testDuration - 1;
       }
       else if (resultsDate.getDay() !== 0 && resultsDate.getDay() !== 6) {
+        // All other tests do not occur over the weekend so do not decrement the
+        // test duration if the result date is a Saturday or Sunday
         testDuration = testDuration - 1;
       }
     }
